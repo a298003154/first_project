fd10fa6c1ee775f22b7cb6a37f2848c80f0271cc
a298003154 <298003154@qq.com>
2018-03-22 23:22:26 +0800

    update

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,10 +1,7 @@
-
 module.exports = {
-    plugins: (loader) => [
-        //CSS浏览器兼容
-        require('postcss-import'),
-        require('autoprefixer')(
-            { browsers: ['last 5 versions'] }
-        )
-    ]
-}
+  plugins: loader => [
+    //CSS浏览器兼容
+    require("postcss-import"),
+    require("autoprefixer")({ browsers: ["last 5 versions"] })
+  ]
+};
